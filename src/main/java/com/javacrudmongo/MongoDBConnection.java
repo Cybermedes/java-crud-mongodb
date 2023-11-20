5269cf6640fcca0f6e9212ab8b2fba9515709533
Lucas Paiva <lpaivamatiazzi@gmail.com>
2023-11-20 20:06:09 -0300

    Create class to connect to MongoDB using Singleton

---
+++ src/main/java/com/javacrudmongo/MongoDBConnection.java
@@ -0,0 +1,28 @@
+package com.javacrudmongo;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+public class MongoDBConnection {
+
+    private static MongoDBConnection instance = null;
+    private MongoClient mongoClient;
+
+    private MongoDBConnection() {
+        String database = "mongodb://rootuser:rootpass@localhost:27017/?authMechanism=DEFAULT";
+        ConnectionString connectionString = new ConnectionString(database);
+        mongoClient = MongoClients.create(connectionString);
+    }
+
+    public static synchronized MongoDBConnection getInstance() {
+        if (instance == null) {
+            instance = new MongoDBConnection();
+        }
+        return instance;
+    }
+
+    public MongoClient getMongoClient() {
+        return mongoClient;
+    }
+}
